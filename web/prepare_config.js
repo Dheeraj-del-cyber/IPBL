a342d9a9f8f0de76cec3cce0a2824e9964a8e3a5
R1007 <dheerajkoila97@gmail.com>
2026-05-26 22:36:09 +0530

    Set default API_BASE to Render backend URL

--- web/prepare_config.js
+++ web/prepare_config.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-const apiBase = process.env.API_BASE || "";
+const apiBase = process.env.API_BASE || "https://ipbl-backend-c9ug.onrender.com";
 
 const content = `window.API_BASE = "${apiBase}";`;
 
